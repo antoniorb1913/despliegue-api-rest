37da35059cc2a87e10f91e5d04b56796292a3e7c
antoniorb1913 <antoniorb1913@gmail.com>
2025-09-25 22:20:51 +0200

    registros

--- src/main/java/com/hellin/demo/controller/PetController.java
+++ src/main/java/com/hellin/demo/controller/PetController.java
@@ -1,6 +1,7 @@
 package com.hellin.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import com.hellin.demo.entity.Pet;
@@ -8,6 +9,7 @@ import com.hellin.demo.repository.PetRepository;
 
 
 @RestController
+@RequestMapping("/pet")
 public class PetController {
 
 
